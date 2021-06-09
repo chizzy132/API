a61c741c0941f99f6426f7b7773809fb29ffc32e
chizzy132 <84761096+chizzy132@users.noreply.github.com>
2021-06-09 15:29:02 +0400

    Update Person.java

--- ServletApplication/src/main/java/API/Person.java
+++ ServletApplication/src/main/java/API/Person.java
@@ -1,111 +1,78 @@
 package API;
 
 public class Person {
-	private String firstname;
-	private String lastname;
-	private String phonenumber;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
 	private String email;
 	private Address[] addresses;
 	private String about;
 	private int birthYear;
 	
-	 
-	
-	public Person(String firstname, String lastname, String phonenumber, String email,  Address[] addresses, String about, int birthYear) {
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.phonenumber = phonenumber;
+	public Person(String firstName, String lastName, String phoneNumber, String email,  Address[] addresses, String about, int birthYear) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
 		this.email = email;
 		this.addresses = addresses;
 		this.about = about;
 		this.birthYear = birthYear;
 	}
 
-
-
-	public String getFirstname() {
-		return firstname;
+	public String getFirstName() {
+		return firstName;
 	}
 
-
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-
-
-	public String getLastname() {
-		return lastname;
+	public String getLastName() {
+		return lastName;
 	}
 
-
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
-
-
-	public String getPhonenumber() {
-		return phonenumber;
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
 
-
-
-	public void setPhonenumber(String phonenumber) {
-		this.phonenumber = phonenumber;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public Address[] getAddresses() {
 		return addresses;
 	}
 
-
-
 	public void setAddresses(Address[] addresses) {
 		this.addresses = addresses;
 	}
-
-
 
 	public String getAbout() {
 		return about;
 	}
 
-
-
 	public void setAbout(String about) {
 		this.about = about;
 	}
-
-
 
 	public int getBirthYear() {
 		return birthYear;
 	}
 
-
-
 	public void setBirthYear(int birthYear) {
 		this.birthYear = birthYear;
 	}
-
-	
-
 	
 }
